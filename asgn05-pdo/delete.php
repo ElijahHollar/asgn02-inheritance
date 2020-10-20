4d22630531ce41fb427b56991bbed739a4038183
Elijah Hollar <elijahshollar@students.abtech.edu>
2020-10-20 14:28:48 -0400

    Completed tutorial 8

---
+++ asgn05-pdo/delete.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'connect.php';
+
+$stmt = $db->prepare("DELETE FROM names WHERE firstname = :firstname");
+
+$stmt->bindValue(":firstname", 'Jenny');
+$stmt->execute();
+
+
+
+
+?>
