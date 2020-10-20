e1bcaa0f056bdd90c55a08e5e481405fa88de16b
Elijah Hollar <elijahshollar@students.abtech.edu>
2020-10-20 13:26:19 -0400

    Completed tutorial 3

--- asgn05-pdo/connect.php
+++ asgn05-pdo/connect.php
@@ -2,11 +2,12 @@
 
 try {
   $db = new PDO( 'mysql:host=localhost;dbname=testdb;charset=utf8', 'root', '' );
-  var_dump($db);
+  // var_dump($db);
 }
 
 catch(Exception $e) {
-  echo $e->getMessage();
+  // echo $e->getMessage();
+  echo "An error has occurred";
 }
 
 ?>
