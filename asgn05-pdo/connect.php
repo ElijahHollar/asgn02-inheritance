70fa30e37a2587cab3bad284ef012633a061a792
Elijah Hollar <elijahshollar@students.abtech.edu>
2020-10-20 13:21:58 -0400

    Completed tutuorial 2

--- asgn05-pdo/connect.php
+++ asgn05-pdo/connect.php
@@ -1,7 +1,12 @@
 <?php
 
-$db = new PDO( 'mysql:host=localhost;dbname=testdb;charset=utf8', 'root', '' );
+try {
+  $db = new PDO( 'mysql:host=localhost;dbname=testdb;charset=utf8', 'root', '' );
+  var_dump($db);
+}
 
-var_dump($db);
+catch(Exception $e) {
+  echo $e->getMessage();
+}
 
 ?>
